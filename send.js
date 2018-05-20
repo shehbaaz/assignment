aa07f236f490982b8cefba323913344300ada107
shehbaaz <39292663+shehbaaz@users.noreply.github.com>
2018-05-20 23:54:29 +0530

    Add files via upload

---
+++ send.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+var amqp = require('amqplib/callback_api');
+var i=1;
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'hello';
+    var msg = 'Hello World!';
+
+    ch.assertQueue(q, {durable: false});
+    // Note: on Node 6 Buffer.from(msg) should be used
+	for(i=0;i<=5;i++)
+	{
+    		ch.sendToQueue(q, new Buffer(msg));
+    		console.log(" [x] Sent %s", msg);
+	}
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
